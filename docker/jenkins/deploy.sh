252ed10f4f0569f337017cdb4033aac820f33990
Santiago <santiagopereyra2702@gmail.com>
2024-11-15 04:02:58 +0000

    Finaly pipeline

--- docker/jenkins/deploy.sh
+++ docker/jenkins/deploy.sh
@@ -54,42 +54,42 @@ for container in "${containers[@]}"; do
     fi
 done
 
-# docker run -d --name $REDIS_CONTAINER_NAME -v redis:/data --network my_app $REDIS_IMAGE_NAME
-# docker run -d --name $MYSQL_CONTAINER_NAME -v mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD --network my_app $MYSQL_IMAGE_NAME
-# docker run -d --name $API_CONTAINER_NAME --env-file /tmp/.env --network my_app $API_IMAGE_NAME
-# docker run -d --name $NGINX_CONTAINER_NAME --env-file /tmp/.env --network my_app $NGINX_IMAGE_NAME
-# docker run -d --name $FRONTEND_CONTAINER_NAME --network my_app $FRONTEND_IMAGE_NAME
-# docker run -d --name $PROXY_CONTAINER_NAME -p 80:80 -p 443:443 --network my_app $PROXY_IMAGE_NAME
+docker run -d --name $REDIS_CONTAINER_NAME -v redis:/data --network my_app $REDIS_IMAGE_NAME
+docker run -d --name $MYSQL_CONTAINER_NAME -v mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD --network my_app $MYSQL_IMAGE_NAME
+docker run -d --name $API_CONTAINER_NAME --env-file /tmp/.env --network my_app $API_IMAGE_NAME
+docker run -d --name $NGINX_CONTAINER_NAME --env-file /tmp/.env --network my_app $NGINX_IMAGE_NAME
+docker run -d --name $FRONTEND_CONTAINER_NAME --network my_app $FRONTEND_IMAGE_NAME
+docker run -d --name $PROXY_CONTAINER_NAME -p 80:80 -p 443:443 --network my_app $PROXY_IMAGE_NAME
 
-# start_time=$(date +%s)
-# while true; do
+start_time=$(date +%s)
+while true; do
 
-#     if docker exec mysql mysql -u"$DB_USER" -p"$DB_PASSWORD" -e "SELECT 1;" >/dev/null 2>&1; then
-#         echo "MySQL está listo para aceptar conexiones."
-#         break
-#     else
-#         echo "MySQL no está listo aún, esperando..."
-#     fi
+    if docker exec mysql mysql -u"$DB_USER" -p"$DB_PASSWORD" -e "SELECT 1;" >/dev/null 2>&1; then
+        echo "MySQL está listo para aceptar conexiones."
+        break
+    else
+        echo "MySQL no está listo aún, esperando..."
+    fi
 
-#     ## Verificar si se ha excedido el tiempo de espera máximo
-#     current_time=$(date +%s)
-#     elapsed_time=$((current_time - start_time))
+    ## Verificar si se ha excedido el tiempo de espera máximo
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
 
-#     if [ "$elapsed_time" -ge "$MAX_WAIT" ]; then
-#         echo "Se agotó el tiempo de espera. MySQL no está listo."
-#         exit 1
-#     fi
+    if [ "$elapsed_time" -ge "$MAX_WAIT" ]; then
+        echo "Se agotó el tiempo de espera. MySQL no está listo."
+        exit 1
+    fi
 
-#     # Esperar antes de volver a intentar
-#     sleep "$WAIT_INTERVAL"
-# done
+    # Esperar antes de volver a intentar
+    sleep "$WAIT_INTERVAL"
+done
 
-# docker exec $API_CONTAINER_NAME php artisan key:generate
-# docker exec $API_CONTAINER_NAME php artisan storage:link
-# docker exec $API_CONTAINER_NAME php artisan optimize:clear
-# docker exec $API_CONTAINER_NAME php artisan down
-# docker exec $API_CONTAINER_NAME php artisan migrate --force
-# docker exec $API_CONTAINER_NAME php artisan config:cache
-# docker exec $API_CONTAINER_NAME php artisan route:cache
-# docker exec $API_CONTAINER_NAME php artisan view:cache
-# docker exec $API_CONTAINER_NAME php artisan up
+docker exec $API_CONTAINER_NAME php artisan key:generate
+docker exec $API_CONTAINER_NAME php artisan storage:link
+docker exec $API_CONTAINER_NAME php artisan optimize:clear
+docker exec $API_CONTAINER_NAME php artisan down
+docker exec $API_CONTAINER_NAME php artisan migrate --force
+docker exec $API_CONTAINER_NAME php artisan config:cache
+docker exec $API_CONTAINER_NAME php artisan route:cache
+docker exec $API_CONTAINER_NAME php artisan view:cache
+docker exec $API_CONTAINER_NAME php artisan up
